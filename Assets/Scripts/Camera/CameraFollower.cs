31e10a994d92690eaa54e1049a7979814b17d6de
zxcarnage <nmoysenovich@gmail.com>
2024-02-01 16:15:39 +0300

    debug + follow

---
+++ Assets/Scripts/Camera/CameraFollower.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Player;
+using UnityEngine;
+
+namespace Camera
+{
+    public class CameraFollower : MonoBehaviour
+    {
+        [SerializeField] private PlayerController _player;
+        [SerializeField] private float _xOffset;
+
+        private void LateUpdate()
+        {
+            transform.position = new Vector3(_player.transform.position.x + _xOffset, 
+                transform.position.y,
+                transform.position.z);
+        }
+    }
+}
